e86e2508092879e77ae61f43fae4d2ffade8bee4
theofylaktos <theofylaktos.a@gmail.com>
2020-02-09 08:37:58 +0200

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'nodeJS',
-        password : 'M0n&',
-        database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 app.use(express.json());
