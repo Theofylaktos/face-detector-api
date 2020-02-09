9d19f8958338bd3e2386ef95385c92bad1fcabbc
theofylaktos <theofylaktos.a@gmail.com>
2020-02-09 07:59:25 +0200

    changing port

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 app.put('/image', (req,res) => {image.handleImage(req, res, db)});
-app.listen(3000, ()=> {
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`App is running on port ${process.env.PORT}`);
 });
